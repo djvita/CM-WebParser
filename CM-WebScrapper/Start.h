a5bc1c66e07db44db0e73b9e9e176a63c966d7b3
Walter Gonzalez Domenzain <walter.gonzalez@smartplace.mx>
2015-08-15 11:42:49 -0500

    Image and header parsed from HTML

--- CM-WebScrapper/Start.h
+++ CM-WebScrapper/Start.h
@@ -7,6 +7,9 @@
 //
 
 #import <UIKit/UIKit.h>
+#import <WebKit/WebKit.h>
+#import "UIImageView+WebCache.h"
+
 #define nURLNews        @"http://doradosfc.com.mx/noticias-del-club.html?page=1"
 
 //Debug
@@ -14,6 +17,11 @@
 #define print(x)            if(nDebugEnable){(x);}
 
 @interface Start : UIViewController <UIWebViewDelegate>
+@property (strong, nonatomic) IBOutlet UIImageView *imgNews;
+@property (strong, nonatomic) IBOutlet UILabel *lblHeader;
+
+
+- (IBAction)btnPressed:(id)sender;
 
 @end
 
--- CM-WebScrapper/Start.m
+++ CM-WebScrapper/Start.m
@@ -16,15 +16,55 @@
 
 - (void)viewDidLoad {
     [super viewDidLoad];
-    [self initController];
-}
 
+}
 - (void)didReceiveMemoryWarning {
     [super didReceiveMemoryWarning];
     // Dispose of any resources that can be recreated.
 }
 
+- (void)viewDidAppear:(BOOL)animated {
+    [super viewDidAppear:animated];
+    [self initController];
+    
+}
 - (void)initController {
+}
+/**********************************************************************************************/
+#pragma mark - Web methods
+/**********************************************************************************************/
+- (void)webViewDidFinishLoad:(UIWebView *)webView {
+    @try {
+    NSLog(@"webViewDidFinishLoad");
+    NSString *stCurrentURL      = self.webNews.request.URL.absoluteString;
+    print(NSLog(@"stCurrentURL %@", stCurrentURL))
+        
+    NSString *count = [self.webNews stringByEvaluatingJavaScriptFromString:@"document.getElementsByClassName('listados').length;"];
+    print(NSLog(@"count %@", count))
+        
+    NSString *stClass       = [self.webNews stringByEvaluatingJavaScriptFromString:[NSString stringWithFormat:@"document.getElementsByClassName('listados')[0].innerHTML;"]];
+    print(NSLog(@"stClassFinish %@", stClass))
+        
+    NSString *stClassHeader = [self.webNews stringByEvaluatingJavaScriptFromString:[NSString stringWithFormat:@"document.getElementsByClassName('listados')[0].children[0].innerText;"]];
+    print(NSLog(@"stClassHeader %@", stClassHeader))
+        
+    NSString *stClassImage  = [self.webNews stringByEvaluatingJavaScriptFromString:[NSString stringWithFormat:@"document.getElementsByClassName('imagen')[0].children[0].getAttribute('src');"]];
+    print(NSLog(@"stClassImage %@", stClassImage))
+        
+    NSURL *url                      = [NSURL URLWithString:stClassImage];
+    [self.imgNews sd_setImageWithURL:url placeholderImage:[UIImage imageNamed:@""] options:SDWebImageRefreshCached];
+        
+    self.lblHeader.text = stClassHeader;
+    self.lblHeader.adjustsFontSizeToFitWidth = YES;
+    }
+    @catch (NSException * e) {print(NSLog(@"Exception"));}
+}
+//-------------------------------------------------------------------------------
+/*- (void)webView:(UIWebView*)webView didFailLoadWithError:(NSError*)error {
+    NSLog(@"didFailLoadWithError");
+}*/
+
+- (IBAction)btnPressed:(id)sender {
     [self.webNews stopLoading];
     self.webNews                = [[UIWebView alloc] init];
     self.webNews.hidden         = YES;
@@ -34,29 +74,4 @@
     self.webNews.delegate       = self;
     [self.webNews loadRequest:requestObj];
 }
-
-/**********************************************************************************************/
-#pragma mark - Web methods
-/**********************************************************************************************/
-- (void)webViewDidFinishLoad:(UIWebView *)webView {
-    @try {
-    NSLog(@"webViewDidFinishLoad");
-    NSString *stCurrentURL      = self.webNews.request.URL.absoluteString;
-    }
-    @catch (NSException * e) {print(NSLog(@"Exception"));}
-}
-//-------------------------------------------------------------------------------
-/*- (void)webView:(UIWebView*)webView didFailLoadWithError:(NSError*)error {
-    NSLog(@"didFailLoadWithError");
-}*/
-
-
-
-
-
-
-
-
-
-
 @end
